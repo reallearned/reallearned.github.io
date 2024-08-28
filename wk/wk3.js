a2a8380398ccb69a635a197186af825bfa3e021f
root <91960533+DerpmanDev@users.noreply.github.com>
2024-08-28 19:19:19 -0400

    no bruv

--- wk/wk3.js
+++ wk/wk3.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/edu/',
-    bare: 'http://66.23.193.126:8080/ov/',
+    bare: 'https://englishclass.photo-frame.com/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/wk/wk1.js', // handler
