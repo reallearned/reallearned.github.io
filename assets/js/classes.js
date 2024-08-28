3c61ae6c761f1fc7b0cfb7cab380534fb2dff04c
root <91960533+DerpmanDev@users.noreply.github.com>
2024-08-28 19:05:30 -0400

    fix this too

--- assets/js/classes.js
+++ assets/js/classes.js
@@ -1,7 +1,5 @@
 window.onload = function() {
     let url = localStorage.getItem('encodedUrl');
     var site = document.getElementById('site');
-
-    url = Ultraviolet.codec.xor.decode(url);
     site.src = '/edu/' + url;
 }
